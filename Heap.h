d516aaf8521ec5f51dfabbdbe1f86e39a694a890
Calvin Choi <cchoi11@cs.binghamton.edu>
2024-11-25 16:44:16 -0500

    moved the percolate functions to public

--- Heap.h
+++ Heap.h
@@ -6,14 +6,14 @@
 
 class Heap{
     private:
-        void percolateDown(int nodeIndex, vector<Voter*> vec); //helper function for remove
-        void percolateUp(int nodeIndex, vector<Voter*> vec); //Helper function for insert
         void heapify(vector<Voter*> vec); //called on vector to turn vector into heap
         vector<Voter*> voterVector;
     public:
         Heap(const vector<Voter*>& vec);
         Voter* extractMax(); //returns voter with greatest impact and reheapify
         void insert(Voter* v); //has to handle resizing vector
+        void percolateDown(int nodeIndex, vector<Voter*> vec); //helper function for remove
+        void percolateUp(int nodeIndex, vector<Voter*> vec); //Helper function for insert
 };
 
 #endif
